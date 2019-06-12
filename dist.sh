0468fb92bdc9b4d1d61f134eb1d33b3d1fd22f62
David Calhoun <david.b.calhoun@gmail.com>
2019-06-12 10:11:33 -0700

    upgrade babel, fix package vuln (braces)

--- dist.sh
+++ dist.sh
@@ -1,6 +1,4 @@
 mkdir dist || true
 
-alias babel="npm-exec babel-cli"
-
-$(npm bin)/babel jstoxml.js --out-file dist/jstoxml.js
-$(npm bin)/uglifyjs dist/jstoxml.js -ecma=5 -o dist/jstoxml-min.js
+npx babel jstoxml.js --out-file dist/jstoxml.js
+npx uglifyjs dist/jstoxml.js -ecma=5 -o dist/jstoxml-min.js
